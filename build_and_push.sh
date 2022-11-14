160adea1464cda6d965dc6746b757ac588cb83a4
Matheus Yamashiro <51720339+yamashirl@users.noreply.github.com>
2022-11-14 12:11:16 -0300

    Adicionado k8s manifests e atualizado script de build

--- build_and_push.sh
+++ build_and_push.sh
@@ -15,14 +15,26 @@ FULL_FRONTEND_TAG=$ECR_URL/$FRONTEND_NAME:$FRONTEND_TAG
 
 
 echo "Criação e registro das imagens"
-
 docker build database/. -t $FULL_DATABASE_TAG && \
 docker push $FULL_DATABASE_TAG
 
-docker build database/. -t $FULL_BACKEND_TAG && \
+docker build backend/. -t $FULL_BACKEND_TAG && \
 docker push $FULL_BACKEND_TAG
 
-docker build database/. -t $FULL_FRONTEND_TAG && \
+docker build frontend/. -t $FULL_FRONTEND_TAG && \
 docker push $FULL_FRONTEND_TAG
 
+echo "Configuração dos Volumes"
+kubectl apply -f persistency.yml
+
+echo "Configuração dos Serviços"
+kubectl apply -f services.yml
+
+echo "Configuração dos Deployments"
+
+sed -i "s/DATABASE_IMAGE/$ECR_URL\/$DATABASE_NAME:$DATABASE_TAG/" deployments.yml
+sed -i "s/BACKEND_IMAGE/$ECR_URL\/$BACKEND_NAME:$BACKEND_TAG/" deployments.yml
+sed -i "s/FRONTEND_IMAGE/$ECR_URL\/$FRONTEND_NAME:$FRONTEND_TAG/" deployments.yml
+
+kubectl apply -f deployments.yml
 
